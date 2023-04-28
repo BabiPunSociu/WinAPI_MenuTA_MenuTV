d02281b10a7951f0e3c1b96723686e8a3444ccee
BabiPunSociu <Nguyendung281002@gmail.com>
2023-04-28 13:22:11 +0700

    Dũng

---
+++ Bai1_3_MenuTA_MenuTV/Bai1_3_MenuTA_MenuTV.cpp
@@ -0,0 +1,170 @@
+﻿#include "framework.h"
+#include "Bai1_3_MenuTA_MenuTV.h"
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+HMENU mnuTA, mnuTV;
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_BAI13MENUTAMENUTV, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_BAI13MENUTAMENUTV));
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+    wcex.hCursor        = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_CURSOR1));  // Tạo chuột khác
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_ICON2));
+
+    return RegisterClassExW(&wcex);
+}
+
+// Khởi tạo menu bằng LoadMenu
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+   mnuTA = LoadMenu(hInst, MAKEINTRESOURCE(IDC_TA));
+   mnuTV = LoadMenu(hInst, MAKEINTRESOURCE(IDR_TV));
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, mnuTA, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    static POINT point; // Lưu vị trí click chuột
+    static HMENU menupopup;
+    switch (message)
+    {
+    case WM_RBUTTONDOWN:
+        menupopup = GetSubMenu(mnuTA, 1);
+        point.x = LOWORD(lParam);
+        point.y = HIWORD(lParam);
+        ClientToScreen(hWnd, &point);   // Kích hoạt vị trí point
+        TrackPopupMenu(menupopup, TPM_RIGHTBUTTON, point.x, point.y, 0, hWnd, NULL);    // hiển thị menupopup
+        break;
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Parse the menu selections:
+            switch (wmId)
+            {
+            case ID_English:
+            case ID_ENG:
+                SetMenu(hWnd, mnuTA);
+                break;
+            case ID_VietNamese:
+            case ID_VIE:
+                SetMenu(hWnd, mnuTV);
+                break;
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: Add any drawing code that uses hdc here...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
---
+++ Bai1_3_MenuTA_MenuTV/resource.h
@@ -0,0 +1,46 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Bai1_3_MenuTA_MenuTV.rc
+//
+#define IDC_MYICON                      2
+#define IDD_BAI13MENUTAMENUTV_DIALOG    102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_BAI13MENUTAMENUTV           107
+#define IDI_SMALL                       108
+#define IDC_BAI13MENUTAMENUTV           109
+#define IDC_TA                          109
+#define IDR_MAINFRAME                   128
+#define IDI_ICON1                       129
+#define IDI_ICON2                       130
+#define IDR_TV                          131
+#define IDC_CURSOR1                     132
+#define ID_FILE_NEW                     32771
+#define ID_FILE_OPEN                    32772
+#define ID_FILE_SAVE                    32773
+#define ID_LANGUAGES_VIETNAMESE         32774
+#define ID_English                      32775
+#define ID_VietNamese                   32776
+#define ID_T32777                       32777
+#define ID_T32778                       32778
+#define ID_T32779                       32779
+#define ID_T32780                       32780
+#define ID_NG32781                      32781
+#define ID_NG32782                      32782
+#define ID_ENG                          32783
+#define ID_VIE                          32784
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32785
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
